b7119e5c6d4754db3e4e670bac814099ac15c0bf
aoaoshin <98395346+aoaoshin@users.noreply.github.com>
2022-01-31 23:13:28 +0900

    アロー関数追加

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@
  * アロー関数
  */
 // 従来の関数
-function func1(str) {
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
   return str;
-}
+};
 console.log(func1("func1です。"));
+
+// アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です。"));
