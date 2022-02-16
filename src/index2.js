8d51bf6b623ee3452f7382447f5f081cecb0d520
aoaoshin <98395346+aoaoshin@users.noreply.github.com>
2022-02-16 21:04:03 +0900

    セクション４まで

--- src/index2.js
+++ src/index2.js
@@ -37,3 +37,41 @@ console.log(newNameArr);
 /**
  * 三項演算子
  */
+const val1 = 1 > 0 ? "trueです。" : "falseです。";
+console.log(val1);
+
+const num = "1300";
+console.log(num.toLocaleString());
+
+const formattedNum =
+  typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています。" : "大丈夫です。";
+};
+console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味
+ */
+const flag1 = true;
+const flag2 = false;
+
+if (flag1 || flag2) {
+  console.log("１か２はtrueになります。");
+}
+if (flag1 && flag2) {
+  console.log("１も２もtrueになります。");
+}
+
+// ||は左がfalseならば右側を返す。
+//  ※ javascriptはnullはfalseと「なる。
+const num1 = null;
+const fee = num1 || "金額未設定です。";
+console.log(fee);
+
+// &&は左がtrueなら左側を返す.
+const num2 = "123";
+const fee2 = num2 && "何か設定されました。";
+console.log(fee2);
